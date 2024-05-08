f503391a1c12f00f36ed0250fd007a5781ec3cd8
rcargom96 <167981192+rcargom96@users.noreply.github.com>
2024-05-08 18:10:44 +0200

    Actualización 8 de Abril

--- js/formulario.js
+++ js/formulario.js
@@ -1,17 +1,17 @@
-const formOpenBtn= document.querySelector("#form-open"),
+const formAbrirBtn= document.querySelector("#form-open"),
 formularios= document.querySelector(".formularios"),
-formContainer= document.querySelector(".form_container"),
-formCloseBtn= document.querySelector(".form_close"),
-signupBtn= document.querySelector("#signup"),
-loginBtn= document.querySelector("#login");
+formCont= document.querySelector(".form_cont"),
+formCerrarBtn= document.querySelector(".form_cerrar"),
+registroBtn= document.querySelector("#registro"),
+inicioBtn= document.querySelector("#inicio");
 
-formOpenBtn.addEventListener("click", () => formularios.classList.add("show"))
-formCloseBtn.addEventListener("click", () => formularios.classList.remove("show"))
+formAbrirBtn.addEventListener("click", () => formularios.classList.add("show"))
+formCerrarBtn.addEventListener("click", () => formularios.classList.remove("show"))
 
 
-signupBtn.addEventListener("click", (e) => {e.preventDefault();
-    formContainer.classList.add("active");
+registroBtn.addEventListener("click", (e) => {e.preventDefault();
+    formCont.classList.add("active");
 });
-loginBtn.addEventListener("click", (e) => {e.preventDefault();
-    formContainer.classList.remove("active");
+inicioBtn.addEventListener("click", (e) => {e.preventDefault();
+    formCont.classList.remove("active");
 });
